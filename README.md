f93f93c95886cfe143901321b52196216f68a10f
Sirko2097 <sirko2097@gmail.com>
2020-05-05 23:36:39 +0300

    applied remarks

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Guessing Game
-Make was run Tue May  5 17:27:44 EEST 2020
+Make was run Tue May  5 23:35:19 EEST 2020
 
-The program contains 24 lines of code
+The program contains 30 lines of code
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,4 +1,4 @@
-amount_of_files=$(ls -1 | wc -l)
+amount_of_files=$(ls -1A | wc -l)
 
 function check_variant {
 	if [[ $1 -lt $amount_of_files ]]
@@ -13,6 +13,12 @@ while :
 do
 	echo "Gues, how many files in the directory? Input your variant: "
 	read user_variant
+
+	if [[ !($user_variant =~ ^[0-9]*$) ]]
+	then
+		echo "It is not a number. Try again"
+		continue
+	fi
 
 	if [[ $user_variant -eq $amount_of_files ]]
 	then
